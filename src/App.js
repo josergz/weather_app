b241257067db959512c115375dba81cdb174e180
josergz <jose.rdgz025@hotmail.com>
2022-06-30 23:48:03 -0500

    Mobile: Vista del modulo de inicio de sesion

--- src/App.js
+++ src/App.js
@@ -7,24 +7,6 @@ function App() {
 		<div>
 			<Routes />
 		</div>
-		// <div>
-		// 	<nav>
-		// 		<ul>
-		// 			<li>
-		// 				<Link to="/">Home</Link>
-		// 			</li>
-		// 			<li>
-		// 				<Link to="/login">Login</Link>
-		// 			</li>
-		// 		</ul>
-		// 		<ul>
-		// 			<li>
-		// 				<a href="http://localhost:3000/login">login2</a>
-		// 			</li>
-		// 		</ul>
-		// 	</nav>
-		// 	<Routes />
-		// </div>
 	);
 }
 
