c1eb0256ebbd10936b99734334ca7ebdc212e5ad
josergz <jose.rdgz025@hotmail.com>
2022-07-12 19:37:48 -0500

    Se agregaron media queries para el diseño responsivo de la vista mobil y tablet, ademas se agregó una navbar con un icono y funcionalidad para cerrar sesion

--- src/components/navEncabezado.js
+++ src/components/navEncabezado.js
@@ -1,11 +1,58 @@
 import React from 'react';
 import './style_components/navEncabezado.css';
+import { signOut } from 'firebase/auth';
+import { auth } from '../fire';
 
-function navEncabezado() {
+function navEncabezado({ user, setAuthState, setUser }) {
+	const signOutHandler = () => {
+		signOut(auth)
+			.then(() => {
+				setUser(null);
+				setAuthState('login');
+			})
+			.catch((err) => console.log(err, 'erorxd'));
+	};
+
 	return (
-		<nav className="navbarxd  text-light">
-			<div className="container-fluid">
-				<h3 className="titulonav">Weather App</h3>
+		// <nav className="navbarxd  text-light">
+		// 	<div className="container-fluid">
+		// 		<h3 className="titulonav">Weather App</h3>
+		// 	</div>
+		// </nav>
+		<nav class="navbar navbar-expand-lg bg-light d-flex">
+			<div class="container-fluid d-flex">
+				<a class="navbar-brand p-2 flex-grow-1" href="#">
+					Weather App
+				</a>
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarTogglerDemo02"
+					aria-controls="navbarTogglerDemo02"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+					<form class="d-flex" role="search">
+						{/* <button
+							onClick={signOutHandler}
+							type="subbmin"
+							class="btn btn-outline-success"
+						>
+							Cerrar sesion
+						</button> */}
+						<button
+							onClick={signOutHandler}
+							class="btn btn-outline-success p-2"
+							type="submit"
+						>
+							Cerrar sesión <i class="bi bi-box-arrow-right"></i>
+						</button>
+					</form>
+				</div>
 			</div>
 		</nav>
 	);
--- src/pages/home.js
+++ src/pages/home.js
@@ -4,6 +4,7 @@ import './style/home.css';
 import { signOut } from 'firebase/auth';
 import { auth } from '../fire';
 import { ToastContainer, toast } from 'react-toastify';
+import Nav from '../components/navEncabezado';
 
 function home({ user, setAuthState, setUser }) {
 	const signOutHandler = () => {
@@ -16,46 +17,48 @@ function home({ user, setAuthState, setUser }) {
 	};
 
 	return (
-		<div className="containerxd">
-			<div className="homeContainer">
-				{/* <h3 className="">Bienvenido {user}</h3> */}
-				{/* <Card /> */}
-				{/* <Form /> */}
+		<div>
+			<Nav />
+			<div className="containerxd">
+				<div className="homeContainer">
+					{/* <h3 className="">Bienvenido {user}</h3> */}
+					{/* <Card /> */}
 
-				<div class="card mb-3 cardHome">
-					<div class="row g-0">
-						<div class="col-md-4">
-							{/* <img
+					<div class="card mb-3 cardHome">
+						<div class="row g-0">
+							<div class="col-md-4">
+								{/* <img
 								style={{ height: '100%' }}
 								src="https://source.unsplash.com/category/nature"
 								class="img-fluid rounded-start"
 								alt="imagen"
 							/> */}
-							<img
-								src="https://i.pinimg.com/550x/e7/52/3f/e7523fa41a5bf574adc155807e8b5d00.jpg"
-								class="img-fluid rounded-start"
-								alt="imagen"
-							/>
-						</div>
-						<div class="col-md-8">
-							<div class="card-body">
-								<h5 class="card-title">Bienvenido {user}</h5>
-								<p class="card-text">
-									A traves de Weather App puedes consultar el estado del clima
-									de cualquier ciudad, ingresando su nombre en la barra de
-									busqueda.
-								</p>
+								<img
+									src="https://i.pinimg.com/550x/e7/52/3f/e7523fa41a5bf574adc155807e8b5d00.jpg"
+									class="img-fluid rounded-start imgHome"
+									alt="imagen"
+								/>
+							</div>
+							<div class="col-md-8">
+								<div class="card-body">
+									<h5 class="card-title">Bienvenido {user}</h5>
+									<p class="card-text">
+										A traves de Weather App puedes consultar el estado del clima
+										de cualquier ciudad, ingresando su nombre en la barra de
+										busqueda.
+									</p>
 
-								<Form />
-								{/* Boton para cerrar sesion  */}
-								<div className="">
-									<button
-										onClick={signOutHandler}
-										type="button"
-										class="btn btn-primary mt-8 flex flex-col gap-y-4"
-									>
-										Cerrar sesion
-									</button>
+									<Form />
+									{/* Boton para cerrar sesion  */}
+									<div className="">
+										<button
+											onClick={signOutHandler}
+											type="button"
+											class="btn btn-primary mt-8 flex flex-col gap-y-4"
+										>
+											Cerrar sesion
+										</button>
+									</div>
 								</div>
 							</div>
 						</div>
